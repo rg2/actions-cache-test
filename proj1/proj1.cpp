a4fe801c49ed5e2cba03d4d55092adca37bb486c
Robert Grupp <robert.grupp@gmail.com>
2022-08-06 23:54:57 -0700

    initial project structure

---
+++ proj1/proj1.cpp
@@ -0,0 +1,10 @@
+
+#include "proj1.h"
+
+#include <iostream>
+
+void Proj1Fn()
+{
+  std::cout << "hello form proj1 lib" << std::endl;
+}
+
---
+++ proj2/proj2.cpp
@@ -0,0 +1,14 @@
+
+#include "proj1.h"
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  std::cout << "hello from proj2 main()" << std::endl;
+
+  Proj1Fn();
+
+  return 0;
+}
+
