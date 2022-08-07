134efb32564f6984d0396d652d830c3a91e088d2
Robert Grupp <robert.grupp@gmail.com>
2022-08-07 15:07:15 -0700

    adding use of fmt

--- proj2/proj2.cpp
+++ proj2/proj2.cpp
@@ -1,6 +1,8 @@
 
 #include "proj1.h"
 
+#include <fmt/core.h>
+
 #include <iostream>
 
 int main(int argc, char* argv[])
@@ -8,6 +10,8 @@ int main(int argc, char* argv[])
   std::cout << "hello from proj2 main()" << std::endl;
 
   Proj1Fn();
+
+  fmt::print("printed from fmt! {0}", "arg 0");
 
   return 0;
 }
